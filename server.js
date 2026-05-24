f3e078eea44b78ae24402d5843c2fa28cdccf648
ryanazander-cloud <ryan.a.zander@gmail.com>
2026-05-24 16:42:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({origin:'*'}));
 app.use(express.json());
 
 function formatTime(t) {
